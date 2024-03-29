2c1a6a16eece24e97aa5a01e0653d42efe4bc009
Matheus1415 <115950084+Matheus1415@users.noreply.github.com>
2024-03-29 10:20:14 -0300

    testando HOOk

--- src/state/helpers/realizarSorteio.test.ts
+++ src/state/helpers/realizarSorteio.test.ts
@@ -1,7 +1,22 @@
+import { realizarSorteio } from "./realizarSorteio";
 
 describe('dado um sorteio de amigos secretos', ()=>{
 
     test('cada participante não sortei o proprio nome', () =>{
-        const par
+        const participantes = [
+            'matheus',
+            'vitor',
+            'guilerme',
+            'Juan',
+            'Gabriel',
+            'Daniel'
+        ]
+
+        const sorteio = realizarSorteio(participantes);
+        participantes.forEach(participante => {
+            const amigosSecreto = sorteio.get(participante);
+            expect(amigosSecreto).not.toEqual(participante);
+        })
+
     })
 })
